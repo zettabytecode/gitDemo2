dce6af112e8933ab9302343dbba2c7d957859ccc
Ola Babalola <olamarcb@olas-macbook-air.local>
2016-06-23 17:58:15 -0700

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Ola Babalola on 6/23/16.
+//  Copyright © 2016 Ola Babalola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
